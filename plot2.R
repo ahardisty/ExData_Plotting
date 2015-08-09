6cf5b9fde30a76a49e6befa1cc9a2085c9139f17
Aaron Hardisty <aaron.hardisty@gmail.com>
2015-08-08 22:24:12 -0700

    Updated code

--- plot2.R
+++ plot2.R
@@ -1,24 +1,41 @@
 # Plot 2 Data Access ------------------------------------------------------------------
+# Load Packages -----------------------------------------------------------
+library(lubridate) # load lubridate; eaiser to work with dates
+library(dplyr) # easier to select and filter data
+
+# Access Data -------------------------------------------------------------
+# set wd for GitHub Repo sync
+setwd("./Documents/Personal Projects/Data_Science_Training/R Training/EDA_Coursera/") 
+# unzip into working directory; create "PowerData" as target directory
+unzip("../../../../../LocalData/exdata-data-household_power_consumption.zip", exdir = "PowerData")
+# check files in unzipped directory
+list.files("./PowerData/")
+
+# read 1000 rows of data to see structure
+powerDataShort <- read.table("./PowerData/household_power_consumption.txt",
+                             header = TRUE, sep = ";", stringsAsFactors = FALSE, nrows = 1000, blank.lines.skip = TRUE)
+
+# check structure of data frame
+str(powerDataShort)
+
+# read all rows to create data frame
+powerData <- read.table("./PowerData/household_power_consumption.txt", 
+                             header = TRUE, sep = ";", stringsAsFactors = FALSE)
 
 
-setwd("~")
-setwd("./Documents/Personal Projects/Data_Science_Training/R Training/EDA_Coursera/") # set wd for GitHub Repo sync
+# subset data frame
+powerData <- powerData%>% 
+        filter(Date %in% c("1/2/2007","2/2/2007"))
 
-getwd() # show working directory
-list.files() # list files in working directory
-library(lubridate) # load lubridate package
+# check structure of data frame
+str(powerData)
 
-powerData <- read.table("./power_data/household_power_consumption.txt", header = TRUE, sep=";", stringsAsFactors = FALSE) # create data frame
-powerShort <- powerData[ powerData$Date %in% c("1/2/2007","2/2/2007"), ] # subset on specific dates
-
-powerShort$Global_active_power <- as.numeric(powerShort$Global_active_power)
-powerShort$fullTime <- paste(powerDataShort$Date, powerDataShort$Time)
-powerShort$fullTime <- mdy_hms(powerShort$fullTime)
-
-
-
+# Convert to Correct Class ------------------------------------------------
+# change class
+powerData[,c(1:2)] <- sapply(powerData[,c(1,2)], as.character) # change class to character
+powerData[,c(3:9)] <- sapply(powerData[,c(3:9)], as.numeric) # change class to numeric
+powerData$fullTime <- dmy_hms(paste(powerData$Date, powerData$Time)) # create full time variable POSIXct class
 # Plot 2 Creation ---------------------------------------------------------
-
 
 png("./ExData_Plotting/plot2.png", width=480, height=480)
 plot(x = powerShort$fullTime,
